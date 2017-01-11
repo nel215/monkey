226fde095e1919740cb7fd6ecd97b9a7071d44cc
nel215 <otomo.yuhei@gmail.com>
2017-01-11 18:11:30 +0900

    Make the lexer that consider other tokens

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -34,7 +34,23 @@ func (l *Lexer) NextToken() token.Token {
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
 	}
+
+	l.readChar()
 	return tok
 }
 
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -14,6 +14,13 @@ func TestNextToken(t *testing.T) {
 		expectedLiteral string
 	}{
 		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
 	}
 
 	l := New(input)
