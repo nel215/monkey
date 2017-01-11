63888c257adc317cdccbcaa89d6fa29f211b7ee6
nel215 <otomo.yuhei@gmail.com>
2017-01-11 18:03:20 +0900

    Add test for lexer

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -3,8 +3,28 @@ package lexer
 import (
 	"testing"
 
-	_ "github.com/nel215/monkey/token"
+	"github.com/nel215/monkey/token"
 )
 
 func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+	}
 }
