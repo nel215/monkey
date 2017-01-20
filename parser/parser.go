e06cccad3f9a1b74777fb14656473d73101fe835
nel215 <otomo.yuhei@gmail.com>
2017-01-20 18:14:54 +0900

    Add some method to test sample

--- parser/parser.go
+++ parser/parser.go
@@ -15,5 +15,13 @@ type Parser struct {
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l}
 
+	p.nextToken()
+	p.nextToken()
+
 	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
 }
