4107715ba38a966818988648579ba3474576d0f6
nel215 <otomo.yuhei@gmail.com>
2017-01-23 12:45:26 +0900

    Add test to check ParseProgram

---
+++ parser/parser_test.go
@@ -0,0 +1,20 @@
+package parser
+
+import (
+	"github.com/nel215/monkey/lexer"
+	"testing"
+)
+
+func TestLetStatement(t *testing.T) {
+	input := `
+let x = 5;
+`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+}
