dd84b7854d9eaa53d98e935a18706123f56469ba
nel215 <otomo.yuhei@gmail.com>
2017-01-26 12:50:37 +0900

    Add a test to check the number of return statements

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -52,6 +52,25 @@ let foobar = 838383;
 
 }
 
+func TestReturnStatements(t *testing.T) {
+	input := `
+return 5;
+return 10;
+return 993322;
+`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+}
+
 func checkParserErrors(t *testing.T, p *Parser) {
 	errors := p.Errors()
 
