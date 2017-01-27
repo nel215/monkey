45f9e5b3f05f0b40beb9df3a695380a773ec916d
nel215 <otomo.yuhei@gmail.com>
2017-01-28 00:03:54 +0900

    Add a test to check length of statements of IdentifierExpression

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -95,3 +95,16 @@ return 993322;
 	}
 
 }
+
+func TestIdentifierExpression(t *testing.T) {
+	input := "foobar;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d", len(program.Statements))
+	}
+}
