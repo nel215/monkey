49e703b08d563e2e0bd372f4cbe20855aac8ea83
nel215 <otomo.yuhei@gmail.com>
2017-02-08 23:41:17 +0900

    Implement method to inspect integer expression

--- object/object.go
+++ object/object.go
@@ -1,8 +1,15 @@
 package object
 
+import (
+	"fmt"
+)
+
 type Object interface {
+	Inspect() string
 }
 
 type Integer struct {
 	Value int64
 }
+
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
