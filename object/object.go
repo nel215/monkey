b0930dc04609196b2063691fb72b6a1f341b9d20
nel215 <otomo.yuhei@gmail.com>
2017-02-10 12:50:28 +0900

    Add ObjectType

--- object/object.go
+++ object/object.go
@@ -4,23 +4,35 @@ import (
 	"fmt"
 )
 
+type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
 type Object interface {
 	Inspect() string
+	Type() ObjectType
 }
 
 type Null struct {
 }
 
-func (n *Null) Inspect() string { return "null" }
+func (n *Null) Inspect() string  { return "null" }
+func (n *Null) Type() ObjectType { return NULL_OBJ }
 
 type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 
 type Boolean struct {
 	Value bool
 }
 
-func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
