7e22924b742153dd85acf3fecfad64959009059b
nel215 <otomo.yuhei@gmail.com>
2017-02-12 17:25:52 +0900

    Implement == evaluation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -71,6 +71,9 @@ func TestEvalBooleanExpression(t *testing.T) {
 		{"1 != 1", false},
 		{"1 == 2", false},
 		{"1 != 2", true},
+		{"true == true", true},
+		{"false == false", true},
+		{"true == false", false},
 	}
 
 	for _, tt := range tests {
