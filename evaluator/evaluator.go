bc48091611cd27a6a861d6016650e76a5dbdd812
nel215 <otomo.yuhei@gmail.com>
2017-02-13 18:11:10 +0900

    Evaluate NULL and FALSE as false

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -56,8 +56,12 @@ func evalIfExpression(ie *ast.IfExpression) object.Object {
 
 func isTruthy(obj object.Object) bool {
 	switch obj {
+	case NULL:
+		return false
 	case TRUE:
 		return true
+	case FALSE:
+		return false
 	default:
 		return true
 	}
