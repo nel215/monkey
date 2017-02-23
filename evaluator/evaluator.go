9b30057447ef41d2d90b5af3f1c29f12478e0965
nel215 <otomo.yuhei@gmail.com>
2017-02-23 21:59:28 +0900

    Implement variable scope

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -255,7 +255,7 @@ func applyFunction(fn object.Object, args []object.Object) object.Object {
 }
 
 func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
-	env := fn.Env
+	env := object.NewEnclosedEnvironment(fn.Env)
 
 	for paramIdx, param := range fn.Parameters {
 		env.Set(param.Value, args[paramIdx])
