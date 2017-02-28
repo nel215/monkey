d1ec9076a41b20b7cb4162c67e40893cb8ced7f4
nel215 <otomo.yuhei@gmail.com>
2017-02-28 18:07:31 +0900

    Add builtin function object

--- object/object.go
+++ object/object.go
@@ -17,12 +17,15 @@ const (
 	ERROR_OBJ        = "RETURN"
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type Object interface {
 	Inspect() string
 	Type() ObjectType
 }
+
+type BuiltinFunction func(args ...Object) Object
 
 type Null struct {
 }
@@ -89,3 +92,10 @@ type String struct {
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
