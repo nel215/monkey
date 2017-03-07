c8c859e29e46a97743f6ff2507b78d12b51dd54c
nel215 <otomo.yuhei@gmail.com>
2017-03-07 12:51:36 +0900

    Implement the evaluation of array index expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -81,6 +81,16 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return elements[0]
 		}
 		return &object.Array{Elements: elements}
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+		return evalIndexExpression(left, index)
 	}
 
 	return nil
@@ -265,6 +275,26 @@ func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Ob
 	}
 
 	return res
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObj := array.(*object.Array)
+	idx := int(index.(*object.Integer).Value)
+
+	if idx < 0 || idx >= len(arrayObj.Elements) {
+		return NULL
+	}
+
+	return arrayObj.Elements[idx]
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
