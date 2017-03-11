8372d9b3069131ee302fc11130ff7ad237a5ca0a
nel215 <otomo.yuhei@gmail.com>
2017-03-12 00:12:23 +0900

    Add hash object

--- object/object.go
+++ object/object.go
@@ -146,3 +146,28 @@ func (ao *Array) Inspect() string {
 
 	return out.String()
 }
+
+type HashPair struct {
+	Key   Object
+	Value Object
+}
+
+type Hash struct {
+	Pairs map[HashKey]HashPair
+}
+
+func (h *Hash) Type() ObjectType { return HASH_OBJ }
+func (h *Hash) Inspect() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for _, pair := range h.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+
+	return out.String()
+}
